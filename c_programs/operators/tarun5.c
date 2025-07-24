6f3e4b966545a969a3eea6a7505d8f2c55c79110
sasaputarun <tarunsasapu08@gmail.com>
2025-07-24 10:41:26 +0530

    comarision

---
+++ c_programs/operators/tarun5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int m, n;
+	printf("enter two numbers");
+	scanf("%d %d",&m, &n);
+	if(m==n)
+		printf("both are equal");
+	else
+		printf("both are not equal");
+	return 0;
+}
