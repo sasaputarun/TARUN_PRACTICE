99a0f44eee4e4294f5966e1c9bbfd3ba508be497
sasaputarun <tarunsasapu08@gmail.com>
2025-07-28 10:44:34 +0530

    if else

---
+++ c_programs/controlstatements/else2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter the number");
+	scanf("%d",&num);
+	if(num%5==0)
+		printf("num is divisible by 5");
+	else
+		printf("num is not divisible by 5");
+}
