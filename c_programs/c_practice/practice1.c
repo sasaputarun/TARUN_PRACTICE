60f6d39b1655cfc064b1ba83cd4eeaf3c6c0c1c4
sasaputarun <tarunsasapu08@gmail.com>
2025-07-28 14:52:21 +0530

    datatypes

---
+++ c_programs/c_practice/practice1.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>
+int main(){
+	int i=67;
+       float f=2.5;
+       char ch='a';
+       double d=3.14159;
+       printf("integer=%d\n",i);
+       printf("float=%f\n",f);
+       printf("char=%c\n",ch);
+       printf("double=%lf\n",d);
+       return 0;
+}
+       
