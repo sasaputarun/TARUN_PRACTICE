fd5677e0977e4f37766d4d37ebd9cbd5ba942c78
sasaputarun <tarunsasapu08@gmail.com>
2025-07-28 11:11:48 +0530

    if else

---
+++ c_programs/controlstatements/else3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	char ch;
+	printf("enter a character");
+	scanf("%c",&ch);
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+	printf("it is a vowel\n");
+	}
+	else{
+		printf("it is consonent\n");
+	}
+}
