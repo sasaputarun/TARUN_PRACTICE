2c4e9484701821aaf53bf3ff5561941fc84564b8
sasaputarun <tarunsasapu08@gmail.com>
2025-07-29 12:09:29 +0530

    square root

---
+++ c_programs/c_practice/practice12.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float num,result;
+	printf("enter a number:");
+	scanf("%f",&num);
+	result=sqrt(num);
+	printf("result:%.2f",result);
+	return 0;
+}
+
