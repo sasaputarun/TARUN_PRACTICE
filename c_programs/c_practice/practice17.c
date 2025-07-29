246e2e30cc28380305c122c0e0efa4dd9f6e7dc0
sasaputarun <tarunsasapu08@gmail.com>
2025-07-29 15:45:08 +0530

    area

--- c_programs/c_practice/practice17.c
+++ c_programs/c_practice/practice17.c
@@ -2,7 +2,11 @@
 #include<math.h>
 int main()
 {
-	float b=6,h=10,area;
+	float b,h,area;
+	printf("enter the base of a triangle");
+	scanf("%f",&b);
+	printf("enter the height of a triangle");
+	scanf("%f",&h);
 	area=0.5*b*h;
 	printf("area of a triangle:%.2f",area);
 	return 0;
