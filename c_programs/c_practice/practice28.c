3a5efdd6c871ff14d4aaefe15846f5e6c3d583b0
sasaputarun <tarunsasapu08@gmail.com>
2025-07-30 22:33:06 +0530

    Hcf

---
+++ c_programs/c_practice/practice28.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,hcf;
+	printf("enter a and b");
+	scanf("%d %d",&a,&b);
+	int min=(a<b)?a:b;
+	for(int i=1;i<=min;i++){
+		if(a%i==0&&b%i==0){
+			hcf=i;
+		}
+	}
+	printf("hcf:%d",hcf);
+}
