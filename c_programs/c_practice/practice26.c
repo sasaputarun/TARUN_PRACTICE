29667d1da04a4929010cffa9ea24d77bedb862da
sasaputarun <tarunsasapu08@gmail.com>
2025-07-30 16:43:40 +0530

    difference

---
+++ c_programs/c_practice/practice26.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+
+	int num1,num2,difference;
+	printf("enter two numbers num1 and num2");
+	scanf("%d %d",&num1,&num2);
+	difference=num1-num2;
+	printf("difference between numbers:%d",difference);
+	return 0;
+}
