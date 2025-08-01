e8652dc2ce5a0fcc54a9c4043d13005521ee52a0
sasaputarun <tarunsasapu08@gmail.com>
2025-08-01 12:13:49 +0530

    operators

---
+++ c_programs/operators/relationall.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int age=18;
+if(age>=18){
+printf("eligible for vote");
+}
+else{
+printf("not eligible");
+}
+return 0;
+}
+~    
---
+++ c_programs/operators/tarun9.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a=5;
+	printf("a=%d\n %d\n %d\n",a += 3,a-=3,a*=3);
+	return 0;
+}
+
