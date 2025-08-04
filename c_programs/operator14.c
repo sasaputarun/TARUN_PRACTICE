c15a0dece9894ed34e2ce9f4883e967c17a8bcd9
sasaputarun <tarunsasapu08@gmail.com>
2025-08-04 11:13:17 +0530

    area

---
+++ c_programs/operator14.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int base,height,area;
+	printf("enter base and height");
+	scanf("%d %d",&base,&height);
+	area=(base*height)/2;
+	printf("area of a triangle:%d",area);
+	return 0;
+}
