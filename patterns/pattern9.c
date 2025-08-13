2567dd7a7ea35d1df2864ac679729df33388c3fd
sasaputarun <tarunsasapu08@gmail.com>
2025-08-13 17:50:40 +0530

    inverted pyramid

---
+++ patterns/pattern9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int i,j,k;
+	for(i=7;i>=1;i--){
+		for(j=1;j<=(7-i);j++){
+			printf(" ");
+		}
+		for(k=1;k<=(2*i-1);k++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
