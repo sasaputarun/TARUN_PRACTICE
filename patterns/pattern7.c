867177e12bfbb1d851f45773fffdec6ae99869a3
sasaputarun <tarunsasapu08@gmail.com>
2025-08-13 16:49:21 +0530

    rhombus

---
+++ patterns/pattern7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int i,j,k;
+	for(i=1;i<=6;i++){
+		for(j=1;j<=(6-i);j++){
+			printf(" ");
+		}
+		for(k=1;k<=6;k++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
