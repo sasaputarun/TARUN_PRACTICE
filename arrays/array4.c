24d01ab4f5f8747fc5afff646489be4664352ff2
sasaputarun <tarunsasapu08@gmail.com>
2025-08-19 11:47:16 +0530

    using arrays

--- arrays/array4.c
+++ arrays/array4.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
 	int a[6],i;
-	int sum=0,avg;
+	int sum=0;
+	float avg;
 	printf("enter array elements");
 	for(i=0;i<=5;i++){
 		scanf("%d",&a[i]);
@@ -10,5 +11,5 @@ int main(){
 		sum=sum+a[i];
 	}
 	avg=(sum/6);
-		printf("sum:%d\n avg:%d\n",sum,avg);
+		printf("sum:%d\n avg:%.2f\n",sum,avg);
 }
