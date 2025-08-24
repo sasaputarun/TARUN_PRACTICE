8b70a516f7fe9542de2c0881ceb7de48836078fa
sasaputarun <tarunsasapu08@gmail.com>
2025-08-24 18:27:11 +0530

    function

---
+++ functions/gate1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int incr(int i)
+{
+	static int count=0;
+	count=count+i;
+	return count;
+}
+int main()
+{
+	int i,j;
+	for(i=0;i<=4;i++)
+	{
+		j=incr(i);
+	}
+	printf("the final value of j:%d\n",j);
+}
