3f19e775352a2fa89bb57227ef553a7987a5daf6
sasaputarun <tarunsasapu08@gmail.com>
2025-08-30 18:13:48 +0530

    pointer arithmetic

---
+++ pointers/increment.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+        int a[]={1,2,3,45,5};
+        int *p;
+        p=&a[2];
+	//p++;
+        printf("%d\n",*p);
+        printf("%d  %d\n",*p++,*p++);
+        printf("%d",*p);
+}
+
