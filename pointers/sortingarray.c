abdfa5bd33444c551504416f2776a04c853800be
sasaputarun <tarunsasapu08@gmail.com>
2025-08-31 18:05:03 +0530

    sorting array using pointers

---
+++ pointers/sortingarray.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	printf("enter array size:");
+	scanf("%d",&n);
+	int arr[n];
+	int *ptr=arr;
+	printf("enter array elements:");
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",(ptr+i));
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(*(ptr+i)>*(ptr+j))
+			{
+				int temp=*(ptr+i);
+				*(ptr+i)=*(ptr+j);
+				*(ptr+j)=temp;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",*(ptr+i));
+	}
+	return 0;
+}
+			
+
