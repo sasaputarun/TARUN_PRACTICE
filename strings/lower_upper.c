e24d7990033fb0b931015a9672f83e7a7274616f
sasaputarun <tarunsasapu08@gmail.com>
2025-09-05 17:42:17 +0530

    conversion lower into upper

---
+++ strings/lower_upper.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+        int n;
+        printf("enter the character array size:");
+        scanf("%d",&n);
+	while(getchar()!='\n');
+        char str[n];
+        printf("enter the string");
+        fgets(str,n,stdin);
+        for(int i=0;str[i]!='\0';i++)
+        {
+                if(str[i]>='a'&&str[i]<='z')
+                {
+                        str[i]=str[i]-32;
+                }
+        }
+        printf("%s",str);
+        return 0;
+}
+
