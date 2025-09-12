ec1148b3ab695c3267391ddcfaa7ce032bff44fa
sasaputarun <tarunsasapu08@gmail.com>
2025-09-12 10:46:57 +0530

    conversion of decimal into hexadecimal

--- numberconversions/decimal_hexadecimal.c
+++ numberconversions/decimal_hexadecimal.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 int main()
 {
-	int i=0,n,j,binary[32];
+	int i=0,n,j,hexa[32];
 	printf("enter a decimal number");
 	scanf("%d",&n);
 	if(n==0)
 	{
-		printf("binary:%d\n",n);
+		printf("hexa:%d\n",n);
 		return 0;
 	}
 	while(n>0)
 	{
-		binary[i]=n%16;
+		hexa[i]=n%16;
 		n=n/16;
 		i++;
 	}
 	for(j=i-1;j>=0;j--)
 	{
-		printf("%d",binary[j]);
+		printf("%d",hexa[j]);
 	}
 	printf("\n");
+	return 0;
 }
 
