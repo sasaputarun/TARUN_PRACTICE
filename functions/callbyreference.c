f2e97d420d731c41f0f30bd765362289cf778820
sasaputarun <tarunsasapu08@gmail.com>
2025-09-17 13:59:50 +0530

    by passing address as an argument

---
+++ functions/callbyreference.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void fun(int*,int*);
+int main()
+{
+	int x=9,y=4;
+	fun(&x,&y);
+	printf("x:%d  y:%d",x,y);
+}
+void fun(int* x,int* y)
+{
+	*x=8,*y=3;
+	printf("x:%d y:%d\n",*x,*y);
+}
+	
