9ab49fc2dc9634388bd9cd546a9a9d120ac87744
sasaputarun <tarunsasapu08@gmail.com>
2025-10-16 16:23:26 +0530

    reversing array

---
+++ regularpractice/reverse_array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter array size:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter array elements:\n");
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int i=0,j=n-1;
+	while(i<j)
+	{
+		int temp=arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+		i++;
+		j--;
+	}
+	printf("printing the reversed array\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+	return 0;
+}
